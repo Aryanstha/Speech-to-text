90336b38e6a602492bf9e9f5282d9eb88905e215
Aryan Shrestha <67673221+Aryanstha@users.noreply.github.com>
2022-02-02 13:35:53 +0545

    Add files via upload

---
+++ main.js
@@ -0,0 +1,60 @@
+const texts = document.querySelector(".texts");
+
+window.SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+recognition.interimResults = true;
+
+let p = document.createElement("p");
+
+recognition.addEventListener("result", (e) => {
+  texts.appendChild(p);
+  const text = Array.from(e.results)
+    .map((result) => result[0])
+    .map((result) => result.transcript)
+    .join("");
+
+  p.innerText = text;
+  if (e.results[0].isFinal) {
+    if (text.includes("how are you")) {
+      p = document.createElement("p");
+      p.classList.add("replay");
+      p.innerText = "I am fine";
+      texts.appendChild(p);
+    }
+    if (
+      text.includes("what's your name") ||
+      text.includes("what is your name")
+    ) {
+      p = document.createElement("p");
+      p.classList.add("replay");
+      p.innerText = "I am Js";
+      texts.appendChild(p);
+    }
+    if (text.includes("open YouTube")) {
+      p = document.createElement("p");
+      p.classList.add("replay");
+      p.innerText = "opening youtube channel";
+      texts.appendChild(p);
+      console.log("opening youtube");
+      window.open("https://www.youtube.com/");
+    }
+    if (text.includes("open arniko lab page")) {
+      p = document.createElement("p");
+      p.classList.add("replay");
+      p.innerText = "opening page";
+      texts.appendChild(p);
+      console.log("opening page");
+      window.open("https://www.facebook.com/arnikointernationalcollege");
+
+    }
+    p = document.createElement("p");
+  }
+});
+
+recognition.addEventListener("end", () => {
+  recognition.start();
+});
+
+recognition.start();
